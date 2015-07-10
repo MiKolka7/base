94ade67e5da00f17b820281378d6b996b6f6e96a
MiKolka <kolka.koval@yandex.ru>
2015-07-10 23:39:20 +0300

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/* App Module */
+var artApp = angular.module('artApp',
+    [
+        'ngRoute',
+        'ngFileUpload',
+        'ngCookies',
+        'ngAnimate',
+        'chieffancypants.loadingBar'
+    ]);
+
+
+artApp.config(function(cfpLoadingBarProvider) {
+    // true is the default, but I left this here as an example:
+    cfpLoadingBarProvider.includeSpinner = true;
+});
+
+//
+//artApp.config(function (LightboxProvider) {
+//    // set a custom template
+//    LightboxProvider.templateUrl = 'template/modal.html';
+//
+//    // our images array is not in the default format, so we have to write this
+//    // custom method
+//    LightboxProvider.getImageUrl = function (imageUrl) {
+//        return 'images/img/' + imageUrl.src;
+//    };
+//
+//    // set the caption of each image as its text color
+//    //LightboxProvider.getImageCaption = function (imageUrl) {
+//    //    return '#' + imageUrl.src.match(/00\/(\w+)/)[1];
+//    //};
+//
+//    // increase the maximum display height of the image
+//    LightboxProvider.calculateImageDimensionLimits = function (dimensions) {
+//        return {
+//            'maxWidth': dimensions.windowWidth >= 768 ? // default
+//            dimensions.windowWidth - 92 :
+//            dimensions.windowWidth - 52,
+//            'maxHeight': 1600                           // custom
+//        };
+//    };
+//
+//    // the modal height calculation has to be changed since our custom template is
+//    // taller than the default template
+//    LightboxProvider.calculateModalDimensions = function (dimensions) {
+//        var width = Math.max(400, dimensions.imageDisplayWidth + 32);
+//
+//        if (width >= dimensions.windowWidth - 20 || dimensions.windowWidth < 768) {
+//            width = 'auto';
+//        }
+//
+//        return {
+//            'width': width,                             // default
+//            'height': 'auto'                            // custom
+//        };
+//    };
+//});
+
+
+artApp.run(function ($rootScope, $location, $cookieStore) {
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        var url = $location.path().split('/')[1];
+
+        if (url === 'registration') {
+            return false;
+        }
+
+        if (!$cookieStore.get('authorization')) {
+                $location.url("/login");
+        }
+        else {
+            if (!$rootScope.userName) {
+
+                $rootScope.jury = $cookieStore.get('jury');
+                $rootScope.admin = $cookieStore.get('admin');
+                $rootScope.idJury = $cookieStore.get('idJury');
+                $rootScope.userName = $cookieStore.get('login');
+
+            }
+        }
+    });
+});
---
+++ gulpfile.js
@@ -0,0 +1,179 @@
+'use strict';
+
+var   gulp         = require('gulp')
+    , csso         = require('gulp-csso')
+    , notify       = require("gulp-notify")
+    , concat       = require('gulp-concat')
+    , uglify       = require('gulp-uglify')
+    , connect      = require('gulp-connect')
+    , imagemin     = require('gulp-imagemin')
+    , pngquant     = require('imagemin-pngquant')
+    , autoprefixer = require('gulp-autoprefixer')
+
+    // , source     = require('vinyl-source-stream')
+    // , buffer     = require('vinyl-buffer')
+    // , gutil      = require('gulp-util')
+    // , assign     = require('lodash.assign')
+    // , watchify   = require('watchify')
+    // , browserify = require('browserify')
+    // , sourcemaps = require('gulp-sourcemaps')
+    ;
+
+/*
+var customOpts = {
+    entries: ['./app/js/'],
+    noparse: ['app.all', 'app.min'],
+    debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+gulp.task('js', bundle);
+b.on('update', bundle);
+b.on('log', gutil.log);
+
+function bundle() {
+    return b.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist'));
+}
+*/
+
+
+//server
+gulp.task('server', function() {
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
+});
+
+
+//html
+gulp.task('html',function(){
+    gulp.src(['./app/template/**/*.html' ,'index.html'])
+        .pipe(connect.reload())
+        .pipe(notify("Change html"));
+});
+
+
+//css
+gulp.task('css',function(){
+    gulp.src('./app/css/*.css')
+        .pipe(connect.reload())
+        .pipe(notify("Change css"));
+});
+
+
+//js
+gulp.task('js', function() {
+    gulp.src([
+            './app/js/app.js',
+            './app/js/route.js',
+            './app/js/controllers/*.js',
+            './app/js/directives.js',
+            './app/js/filters.js',
+            './app/js/services.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.all.js'))
+        .pipe(gulp.dest('./app/js/'))
+        .pipe(sourcemaps.write())
+        .pipe(connect.reload())
+        .pipe(notify("Change js"));
+});
+
+gulp.task('js-lib', function() {
+    gulp.src([
+            './app/bower_components/angular/angular.js',
+            './app/bower_components/angular-route/angular-route.js',
+            './app/bower_components/angular-cookies/angular-cookies.min.js',
+            './app/bower_components/angular-loading-bar/build/loading-bar.min.js',
+            './app/bower_components/angular-animate/angular-animate.js',
+            './app/js/bower_components/jquery/dist/jquery.js',
+            './app/js/vendor/ng-file-upload-shim.js',
+            './app/js/vendor/ng-file-upload.js',
+            './app/js/vendor/fotorama.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('app-lib.js'))
+        .pipe(gulp.dest('./app/js/'))
+        .pipe(sourcemaps.write())
+        .pipe(connect.reload())
+        .pipe(notify("Change js-lib"));
+});
+
+gulp.task('css-all', function() {
+    gulp.src([
+            './app/css/bootstrap.css',
+            './app/css/theme.css',
+            './app/css/gradients.css',
+            './app/css/fotorama.css',
+            './app/bower_components/angular-loading-bar/build/loading-bar.min.css'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./app/css/'))
+        .pipe(sourcemaps.write())
+        .pipe(connect.reload())
+        .pipe(notify("Change css"));
+});
+
+//clean-js
+gulp.task('clean-js', function() {
+    gulp.src('./app/js/app.all.js')
+        .pipe(clean());
+});
+
+
+//watch
+gulp.task('watch', function () {
+    gulp.watch('./app/template/*', ['html']);
+    gulp.watch('./app/css/**/*', ['css']);
+    //gulp.watch('./app/js/**/*', ['clean-js']);
+    gulp.watch('./app/js/**/*', ['js']);
+});
+
+
+//compress css
+gulp.task('compress-css', function() {
+    return gulp.src('./app/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(csso())
+        .pipe(gulp.dest('./app/css/'));
+});
+
+
+//compress js
+gulp.task('compress-js', function() {
+    gulp.src(['./app/js/app.all.js', './app/js/app-lib.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('./app/js/min/'))
+});
+
+
+//compress image
+gulp.task('compress-image', function () {
+    gulp.src('./app/images/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant({ quality: '50-70', speed: 4 })],
+            interlaced: true
+        }))
+        .pipe(gulp.dest('./app/images/'))
+        .pipe(notify("Compress img"));
+});
+
+
+
+
+gulp.task('default', ['server', 'html', 'css', 'js', 'watch']);
+gulp.task('production', ['js-lib', 'css-all', 'compress-js', 'compress-css', 'compress-image']);
