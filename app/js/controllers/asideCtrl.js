181fd8976fa81d9645615f8be433d7cdb9b4df4a
Dennis Korcheniuk <denniskorch@gmail.com>
2015-10-20 23:39:30 +0300

    aside menu visibility fix

--- app/js/controllers/asideCtrl.js
+++ app/js/controllers/asideCtrl.js
@@ -2,6 +2,10 @@ angular.module('baseApp.controller.aside', [])
     .controller('asideCtrl', function($rootScope, $scope, ngDialog){
     "use strict";
 
+      $rootScope.changeVisibleAside = function() {
+        return $rootScope.visibleAside = $rootScope.visibleAside ? false : true;
+    };
+
     $scope.openWindow = function (name) {
         $rootScope.table = name;
 
